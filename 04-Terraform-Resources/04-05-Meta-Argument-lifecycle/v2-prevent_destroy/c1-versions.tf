a4d0c23497feb42bd29c6a93b21a19ebd92bec82
Manoj KC <unixcap@gmail.com>
2021-09-01 23:04:14 -0500

    Update c1-versions.tf

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v2-prevent_destroy/c1-versions.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v2-prevent_destroy/c1-versions.tf
@@ -1,20 +1,17 @@
-# Terraform Block
+#Terraform Block
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~>1.0.5"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
+      source = "hashicorp/aws"
+
     }
   }
 }
 
-# Provider Block
+#Provider block
 provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
+  region     = "us-east-1"
+  access_key = "AKIAZLW73FYOFR6KUY5C"
+  secret_key = "hH+1b48O+yjSOSsxgJ0RsxUbNuawEvpb3JqY/JRd"
 }
-/*
-Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
-$HOME/.aws/credentials
-*/
